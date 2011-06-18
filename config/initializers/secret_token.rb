2a9ab930d22dc55e27d6727a44bb0d77ee46d201
Christopher King <cwking@mindspring.com>
2011-06-17 20:49:17 -0700

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Googlemaps::Application.config.secret_token = '17ab2dc8d8c7f31047dfcfcf0f3d8dd0bbc7aa0d4c8c55e2f0caa7b5ffb2ef782574ab1cb284243a1ecb4543381ebe7019a343962ca86eb2b9be7b9176eb0f94'
